fc76f9d35593357dc4ba5a7a447ed0e87ebd3ec9
Daniel Onabiyi <45369564+danizzy@users.noreply.github.com>
2023-03-07 16:32:14 +0100

    voice-data

--- controller/text-to-celebrity-voice.js.js
+++ controller/text-to-celebrity-voice.js.js
@@ -1,33 +1,27 @@
 const tf = require('@tensorflow/tfjs-node');
 const { loadGraphModel } = require('@tensorflow/tfjs-converter');
-const { TextToSpeech } = require('@google-cloud/text-to-speech');
+
+// load the saved model from disk
 const model = await loadGraphModel('path/to/model');
-const textToSpeechClient = new TextToSpeech();
 
+// define a function that processes input data using the model
+async function processVoiceData(inputData) {
+  // convert input data to a TensorFlow tensor
+  const inputTensor = tf.tensor2d(inputData);
 
-async function generateCelebritySpeech(textInput) {
-  // generate audio from text using the model
-  const audioData = hdg; // generate audio data using the model
+  // run the model on the input tensor to generate output tensor
+  const outputTensor = model.execute(inputTensor);
 
-  // synthesize speech from the generated audio data using the Text-to-Speech API
-  const [response] = await textToSpeechClient.synthesizeSpeech({
-    input: { text: textInput },
-    voice: { languageCode: 'en-US', name: 'your-celebrity-voice-name' },
-    audioConfig: { audioEncoding: 'MP3' },
-    audioContent: audioData
-  });
+  // convert output tensor to a JavaScript array
+  const outputData = outputTensor.arraySync();
 
-  // return the synthesized speech as a Buffer
-  return response.audioContent;
+  return outputData;
 }
 
+// export the processing function from the module
 module.exports = {
-  generateCelebritySpeech
+  processVoiceData
 };
-
-
-
-
 
 
 
@@ -41,25 +35,66 @@ module.exports = {
 
 // const tf = require('@tensorflow/tfjs-node');
 // const { loadGraphModel } = require('@tensorflow/tfjs-converter');
-
-// // load the saved model from disk
+// const { TextToSpeech } = require('@google-cloud/text-to-speech');
 // const model = await loadGraphModel('path/to/model');
+// const textToSpeechClient = new TextToSpeech();
 
-// // define a function that processes input data using the model
-// async function processVoiceData(inputData) {
-//   // convert input data to a TensorFlow tensor
-//   const inputTensor = tf.tensor2d(inputData);
 
-//   // run the model on the input tensor to generate output tensor
-//   const outputTensor = model.execute(inputTensor);
+// async function generateCelebritySpeech(textInput) {
+//   // generate audio from text using the model
+//   const audioData = hdg; // generate audio data using the model
 
-//   // convert output tensor to a JavaScript array
-//   const outputData = outputTensor.arraySync();
+//   // synthesize speech from the generated audio data using the Text-to-Speech API
+//   const [response] = await textToSpeechClient.synthesizeSpeech({
+//     input: { text: textInput },
+//     voice: { languageCode: 'en-US', name: 'your-celebrity-voice-name' },
+//     audioConfig: { audioEncoding: 'MP3' },
+//     audioContent: audioData
+//   });
 
-//   return outputData;
+//   // return the synthesized speech as a Buffer
+//   return response.audioContent;
 // }
 
-// // export the processing function from the module
 // module.exports = {
-//   processVoiceData
+//   generateCelebritySpeech
 // };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const tf = require('@tensorflow/tfjs-node');
+// // const { loadGraphModel } = require('@tensorflow/tfjs-converter');
+
+// // // load the saved model from disk
+// // const model = await loadGraphModel('path/to/model');
+
+// // // define a function that processes input data using the model
+// // async function processVoiceData(inputData) {
+// //   // convert input data to a TensorFlow tensor
+// //   const inputTensor = tf.tensor2d(inputData);
+
+// //   // run the model on the input tensor to generate output tensor
+// //   const outputTensor = model.execute(inputTensor);
+
+// //   // convert output tensor to a JavaScript array
+// //   const outputData = outputTensor.arraySync();
+
+// //   return outputData;
+// // }
+
+// // // export the processing function from the module
+// // module.exports = {
+// //   processVoiceData
+// // };
